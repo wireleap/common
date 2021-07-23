2362347a5123964e4c86606bfac1dc63debe191a
Anton Pyrogovskyi <anton@wireleap.com>
2021-07-23 15:01:31 +0300

    add versioned interface stubs

---
+++ api/interfaces/relaycontract/version.go
@@ -0,0 +1,11 @@
+// Copyright (c) 2021 Wireleap
+
+package relaycontract
+
+import "github.com/blang/semver"
+
+// The version of this interface is declared here.
+
+const VERSION_STRING = "0.3.3"
+
+var VERSION = semver.MustParse(VERSION_STRING)
