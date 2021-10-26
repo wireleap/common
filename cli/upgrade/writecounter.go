205eb166a1ba8937d096dc2191f292e6f95d8114
Anton Pyrogovskyi <anton@wireleap.com>
2021-10-26 14:22:48 +0300

    upgrade: show download url and local path

--- cli/upgrade/writecounter.go
+++ cli/upgrade/writecounter.go
@@ -5,14 +5,14 @@ package upgrade
 import "fmt"
 
 type WriteCounter struct {
-	name     string
-	n, total int64
+	url, name string
+	n, total  int64
 }
 
 func (wc *WriteCounter) Write(p []byte) (n int, err error) {
 	n = len(p)
 	wc.n += int64(n)
 	perc := int64(float64(wc.n) / float64(wc.total) * 100)
-	fmt.Printf("Downloading %s... %d%%\r", wc.name, perc)
+	fmt.Printf("Downloading %s to %s... %d%%\r", wc.url, wc.name, perc)
 	return
 }
