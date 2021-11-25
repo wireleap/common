e903b170d7832f9326058aea87ba26169268d848
Anton Pyrogovskyi <anton@wireleap.com>
2021-11-25 09:44:32 +0200

    process: always warn about no windows reload

--- cli/process/reload_windows.go
+++ cli/process/reload_windows.go
@@ -3,10 +3,14 @@ package process
 
 import (
 	"errors"
+	"log"
 	"syscall"
 )
 
 var ReloadSignal = syscall.Signal(-1)
 
 // Reload sends the reload signal (SIGUSR1) to the given PID. (NOT IMPLEMENTED YET)
-func Reload(pid int) error { return errors.New("reload on windows not implemented yet") }
+func Reload(pid int) error {
+	log.Printf("reload on windows not implemented yet, please use `wireleap restart` instead")
+	return errors.New("reload on windows not implemented yet")
+}
