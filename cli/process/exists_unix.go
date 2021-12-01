0ce94342cbb702de3a852cfedf6c193853b7c728
Anton Pyrogovskyi <anton@wireleap.com>
2021-12-01 20:12:31 +0200

    process: adjust existence check for windows

---
+++ cli/process/exists_unix.go
@@ -0,0 +1,16 @@
+// +build linux darwin
+
+// Copyright (c) 2021 Wireleap
+package process
+
+import (
+	"os"
+	"syscall"
+)
+
+// Exists checks if a given PID exists.
+func Exists(pid int) bool {
+	// unix can get a permission error if the process is actually alive
+	err := maybeSignal(pid, syscall.Signal(0))
+	return err == nil || os.IsPermission(err)
+}
