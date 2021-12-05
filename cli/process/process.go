6c2d661037c9e4802518913afba22dbe37ff055e
Anton Pyrogovskyi <anton@wireleap.com>
2021-12-05 12:39:46 +0200

    process: ensure proper term signal sent on windows

--- cli/process/process.go
+++ cli/process/process.go
@@ -4,7 +4,6 @@ package process
 
 import (
 	"os"
-	"syscall"
 )
 
 func maybeSignal(pid int, sig os.Signal) error {
@@ -15,11 +14,3 @@ func maybeSignal(pid int, sig os.Signal) error {
 	// unix can only kill 0 if the process is actually alive
 	return p.Signal(sig)
 }
-
-// NOTE: there is no distinction between Term and Kill on windows.
-
-// Term terminates the given PID gracefully.
-func Term(pid int) error { return maybeSignal(pid, syscall.SIGTERM) }
-
-// Kill terminates the given PID forcefully.
-func Kill(pid int) error { return maybeSignal(pid, os.Kill) }
