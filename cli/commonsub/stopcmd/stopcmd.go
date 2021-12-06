52de1fec29fb37b113fcadfda61508c337c73474
Anton Pyrogovskyi <anton@wireleap.com>
2021-12-06 09:17:13 +0200

    stopcmd: adapt to windows

--- cli/commonsub/stopcmd/stopcmd.go
+++ cli/commonsub/stopcmd/stopcmd.go
@@ -19,21 +19,25 @@ func Cmd(arg0 string) *cli.Subcmd {
 		Desc:    fmt.Sprintf("Stop %s daemon", arg0),
 		Run: func(fm fsdir.T) {
 			var (
-				pid int
-				err error
+				pid     int
+				err     error
+				pidfile = arg0 + ".pid"
 			)
-			if err = fm.Get(&pid, arg0+".pid"); err != nil {
+			if err = fm.Get(&pid, pidfile); err != nil {
 				log.Fatalf(
 					"could not get pid of %s from %s: %s",
-					arg0, fm.Path(arg0+".pid"), err,
+					arg0, fm.Path(pidfile), err,
 				)
 			}
-			if err = process.Term(pid); err != nil {
-				log.Fatalf("could not send SIGTERM to %s pid %d: %s", arg0, pid, err)
+			if process.Exists(pid) {
+				if err = process.Term(pid); err != nil {
+					log.Fatalf("could not terminate %s pid %d: %s", arg0, pid, err)
+				}
 			}
 			for i := 0; i < 10; i++ {
 				if !process.Exists(pid) {
 					log.Printf("stopped %s daemon (was pid %d)", arg0, pid)
+					fm.Del(pidfile)
 					return
 				}
 				time.Sleep(100 * time.Millisecond)
@@ -44,6 +48,7 @@ func Cmd(arg0 string) *cli.Subcmd {
 				log.Fatalf("timed out waiting for %s (pid %d) to shut down -- process still alive!", arg0, pid)
 			}
 			log.Printf("stopped %s daemon (was pid %d)", arg0, pid)
+			fm.Del(pidfile)
 		},
 	}
 }
