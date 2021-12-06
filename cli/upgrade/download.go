f26f369b8e0df0167f12a4cf4e86b0e42692298e
Anton Pyrogovskyi <anton@wireleap.com>
2021-12-06 07:56:36 +0200

    upgrade: handle windows .exe extension

--- cli/upgrade/download.go
+++ cli/upgrade/download.go
@@ -4,7 +4,9 @@ package upgrade
 
 import (
 	"fmt"
+	"path/filepath"
 	"runtime"
+	"strings"
 
 	"github.com/blang/semver"
 )
@@ -14,8 +16,18 @@ const (
 	changelogURL = "https://raw.githubusercontent.com/wireleap/%s/master/changelogs/%s.md"
 )
 
+func basename(f string) string { return strings.TrimSuffix(f, filepath.Ext(f)) }
+
+func maybeExe() string {
+	if runtime.GOOS == "windows" {
+		return ".exe"
+	} else {
+		return ""
+	}
+}
+
 func repoName(component string) string {
-	switch component {
+	switch basename(component) {
 	case "wireleap":
 		return "client"
 	case "wireleap-relay":
@@ -27,22 +39,16 @@ func repoName(component string) string {
 	case "wireleap-contract":
 		return "contract"
 	default:
-		return component
+		return basename(component)
 	}
 }
 
 func (u *Config) BinaryURL(ver semver.Version) string {
-	return fmt.Sprintf(releaseURL, repoName(u.binfile), ver.String(), u.binfile, runtime.GOOS, func() string {
-		if runtime.GOOS == "windows" {
-			return ".exe"
-		} else {
-			return ""
-		}
-	}())
+	return fmt.Sprintf(releaseURL, repoName(u.binfile), ver.String(), basename(u.binfile), runtime.GOOS, maybeExe())
 }
 
 func (u *Config) HashURL(ver semver.Version) string {
-	return fmt.Sprintf(releaseURL, repoName(u.binfile), ver.String(), u.binfile, runtime.GOOS, ".hash")
+	return fmt.Sprintf(releaseURL, repoName(u.binfile), ver.String(), basename(u.binfile), runtime.GOOS, maybeExe()+".hash")
 }
 
 func (u *Config) ChangelogURL(ver semver.Version) string {
