8ec665ccb79762703da070615999bfebefc53d7a
Anton Pyrogovskyi <anton@wireleap.com>
2021-12-23 15:31:13 +0200

    transport: return to cloning h2 transport

--- wlnet/transport/transport.go
+++ wlnet/transport/transport.go
@@ -9,7 +9,6 @@ import (
 	"net"
 	"net/http"
 	"net/url"
-	"sync"
 	"time"
 
 	"github.com/wireleap/common/wlnet"
@@ -18,11 +17,7 @@ import (
 
 // T is a complete Wireleap network transport which can dial to other
 // wireleap-relays via H/2 over TCP and targets via TCP or UDP.
-type T struct {
-	*http.Transport
-	mu sync.RWMutex
-	c0 net.Conn
-}
+type T struct{ *http.Transport }
 
 // Options is a struct which contains options for initializing a T.
 type Options struct {
@@ -47,6 +42,8 @@ func New(opts Options) *T {
 		td = &tls.Dialer{NetDialer: nd, Config: tc}
 		t  = &T{
 			Transport: &http.Transport{
+				DialContext:           nd.DialContext,
+				DialTLSContext:        td.DialContext,
 				TLSClientConfig:       tc,
 				ResponseHeaderTimeout: opts.Timeout,
 				ForceAttemptHTTP2:     true,
@@ -57,25 +54,6 @@ func New(opts Options) *T {
 			},
 		}
 	)
-	// if previous connection supplied, use it to tunnel
-	t.Transport.DialContext = func(ctx context.Context, network, host string) (net.Conn, error) {
-		t.mu.RLock()
-		defer t.mu.RUnlock()
-		if t.c0 != nil {
-			return t.c0, nil
-		} else {
-			return nd.DialContext(ctx, network, host)
-		}
-	}
-	t.Transport.DialTLSContext = func(ctx context.Context, network, host string) (net.Conn, error) {
-		t.mu.RLock()
-		defer t.mu.RUnlock()
-		if t.c0 != nil {
-			return tls.Client(t.c0, tc), nil
-		} else {
-			return td.DialContext(ctx, network, host)
-		}
-	}
 	return t
 }
 
@@ -83,17 +61,27 @@ func New(opts Options) *T {
 func (t *T) DialWL(c0 net.Conn, protocol string, remote *url.URL, payload *wlnet.Init) (c net.Conn, err error) {
 	switch remote.Scheme {
 	case "target":
+		// NOTE: this code path is only used by relays
+		// client never dials target directly
 		// c0/payload unused, could both be nil
 		c, err = t.Transport.DialContext(context.TODO(), protocol, remote.Host)
 	case "wireleap":
-		t.mu.Lock()
-		defer t.mu.Unlock()
-		t.c0 = c0
+		tt := t.Transport
+		if c0 != nil {
+			// if previous connection supplied, use it to tunnel
+			tt = t.Transport.Clone()
+			tt.DialContext = func(ctx context.Context, network, host string) (net.Conn, error) {
+				return c0, nil
+			}
+			tt.DialTLSContext = func(ctx context.Context, network, host string) (net.Conn, error) {
+				return tls.Client(c0, t.Transport.TLSClientConfig), nil
+			}
+		}
 		// convert to a stdlib-known scheme
 		u2 := *remote
 		u2.Scheme = "https"
 		// payload used for headers
-		c, err = h2conn.New(t.Transport, u2.String(), payload.Headers())
+		c, err = h2conn.New(tt, u2.String(), payload.Headers())
 	default:
 		err = fmt.Errorf("unsupported dial scheme '%s' in %s", remote.Scheme, remote)
 	}
